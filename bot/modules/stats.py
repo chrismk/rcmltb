4313cf8081bd194a18132ebc6431cb114f1bc77d
Chrismk <chrismk@outlook.com>
2023-07-02 17:21:00 +0800

    Update stats.py

--- bot/modules/stats.py
+++ bot/modules/stats.py
@@ -22,8 +22,8 @@ async def stats(client, message):
     swap = swap_memory()
     memory = virtual_memory()
     stats = f'<b>程序编译时间:</b> {last_commit}\n\n'\
-            f'<b>Bot启动时间:</b> {get_readable_time(time() - botUptime)}\n'\
-            f'<b>系统启动时间:</b> {get_readable_time(time() - boot_time())}\n\n'\
+            f'<b>机器人已运行:</b> {get_readable_time(time() - botUptime)}\n'\
+            f'<b>服务器已运行:</b> {get_readable_time(time() - boot_time())}\n\n'\
             f'<b>硬盘:</b> {get_readable_file_size(total)}\n'\
             f'<b>已用:</b> {get_readable_file_size(used)} | <b>剩余:</b> {get_readable_file_size(free)}\n\n'\
             f'<b>内存:</b> {get_readable_file_size(memory.total)}\n'\
@@ -33,7 +33,6 @@ async def stats(client, message):
             f'<b>CPU:</b> {cpu_percent(interval=0.5)}%\n'\
             f'<b>内存:</b> {memory.percent}%\n'\
             f'<b>硬盘:</b> {disk}%\n\n'\
-            f'<b>CPU核心:</b> {cpu_count(logical=False)}\n'\
             f'<b>交换空间:</b> {get_readable_file_size(swap.total)} | <b>已用:</b> {swap.percent}%\n'
 
     await sendMessage(stats, message)
