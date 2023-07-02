922e48f713b51391ad4edb09b67af82ab68709ba
Chrismk <chrismk@outlook.com>
2023-07-02 12:19:52 +0800

    Update stats.py
    Translate into Chinese

--- bot/modules/stats.py
+++ bot/modules/stats.py
@@ -21,22 +21,22 @@ async def stats(client, message):
     total, used, free, disk = disk_usage('/')
     swap = swap_memory()
     memory = virtual_memory()
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-            f'<b>Bot Uptime:</b> {get_readable_time(time() - botUptime)}\n'\
-            f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
-            f'<b>Total Disk Space:</b> {get_readable_file_size(total)}\n'\
-            f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
-            f'<b>Upload:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-            f'<b>Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
+    stats = f'<b>程序编译时间:</b> {last_commit}\n\n'\
+            f'<b>Bot启动时间:</b> {get_readable_time(time() - botUptime)}\n'\
+            f'<b>系统启动时间:</b> {get_readable_time(time() - boot_time())}\n\n'\
+            f'<b>硬盘总空间:</b> {get_readable_file_size(total)}\n'\
+            f'<b>已使用:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
+            f'<b>已上传:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
+            f'<b>已下载:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
             f'<b>CPU:</b> {cpu_percent(interval=0.5)}%\n'\
-            f'<b>RAM:</b> {memory.percent}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
-            f'<b>Physical Cores:</b> {cpu_count(logical=False)}\n'\
-            f'<b>Total Cores:</b> {cpu_count(logical=True)}\n\n'\
-            f'<b>SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
-            f'<b>Memory Total:</b> {get_readable_file_size(memory.total)}\n'\
-            f'<b>Memory Free:</b> {get_readable_file_size(memory.available)}\n'\
-            f'<b>Memory Used:</b> {get_readable_file_size(memory.used)}\n'
+            f'<b>内存:</b> {memory.percent}%\n'\
+            f'<b>硬盘:</b> {disk}%\n\n'\
+            f'<b>CPU核心:</b> {cpu_count(logical=False)}\n'\
+            f'<b>总核心:</b> {cpu_count(logical=True)}\n\n'\
+            f'<b>交换空间:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
+            f'<b>总内存:</b> {get_readable_file_size(memory.total)}\n'\
+            f'<b>内存剩余:</b> {get_readable_file_size(memory.available)}\n'\
+            f'<b>内存已用:</b> {get_readable_file_size(memory.used)}\n'
     await sendMessage(stats, message)
         
 
